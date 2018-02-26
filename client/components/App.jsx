2aec0ac09fc8200445bcc0e7e3c18f13e30ae8a6
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-27 08:28:23 +1300

    Set up client side to render a message in Spp comonent

---
+++ client/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+
+
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>I am the App</h1>
+      </div>
+    )
+  }
+}
+
+export default App
