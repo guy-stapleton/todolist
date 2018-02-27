7a975e8eca666c3049ba8fea2fe1cf608f073d26
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-28 08:26:48 +1300

    Set up task route to send back an array of tasks

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -3,7 +3,9 @@ const connection = require('../db/connection')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json("this is the end")
+  res.json([{task_id: 1, task_name: 'Pick up drycleaning', notebook_id: 1},
+  {task_id: 2, task_name: 'Get report from Susan', notebook_id: 2},
+  {task_id: 3, task_name: 'Send birthday to Amy', notebook_id: 1}])
 })
 
 module.exports = router
