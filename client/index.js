8513292b51f47cbe5cfc4c9c39beb02aa4dc9d62
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-28 08:21:47 +1300

    Set up redux connection for app

--- client/index.js
+++ client/index.js
@@ -2,13 +2,21 @@ import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
-import thunkMiddlwware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk'
 
 import App from './components/App'
+import reducers from './reducers/index'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(
+  applyMiddleware(thunkMiddleware)
+))
 
 document.addEventListener('DOMContentLoaded', () => {
   render (
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('app')
   )
 })
